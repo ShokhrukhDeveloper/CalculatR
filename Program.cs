93f114ef1ad11113aa54b29ec3459900f5927247
Shoxrux <shoxruxyarashovuz@gmail.com>
2024-02-05 22:58:55 +0500

    Project upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string firstInput = Console.ReadLine();
+string secondInput = Console.ReadLine();
+
+int firstNumber = Convert.ToInt32(firstInput);
+int secondNumber = Convert.ToInt32(secondInput);
+
+
+Console.WriteLine(firstNumber + secondNumber);
